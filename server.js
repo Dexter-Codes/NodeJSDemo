7226208d9f516e0d1de6ea8625724b6b157dc005
Anirban Gupta <anirban.gupta@infor.com>
2022-09-02 23:34:19 +0530

    fIX moongoose error

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose=require('mongoose')
-const db=mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useCreateIndex : true,useUnifiedTopology : true})
-//db.once('error',error=>console.error(error))
-db.once('open',()=>console.log('connected to mongoose'))
+const db=mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology : true})
+//db.on('error',error=>console.error(error))
+//db.once('open',()=>console.log('connected to mongoose'))
 
 
 app.use('/',indexRouter)
