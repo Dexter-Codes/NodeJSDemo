878489c62cfceb28169263fbd4f05019411f80ca
Anirban Gupta <anirban.gupta@infor.com>
2022-09-02 22:51:18 +0530

    fIX dependency error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose=require('mongoose')
-const db=mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+const db=mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useCreateIndex : true})
 db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('connected to mongoose'))
 
