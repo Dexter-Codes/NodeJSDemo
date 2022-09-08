85b655972ddef7edb9e70df4ab80c68fab9bb796
Anirban Gupta <anirban.gupta@infor.com>
2022-09-08 23:30:54 +0530

    fix code

--- routes/books.js
+++ routes/books.js
@@ -117,7 +117,7 @@ async function renderNewPage(res,book,hasError=false)
 function saveCover(book,coverEncoded)
 {
     if(coverEncoded==null) return
-    const cover= JSON.stringify(coverEncoded)
+    const cover= JSON.parse(coverEncoded)
     if(cover!=null && imageMimeTypes.includes(cover.type))
     {
         book.coverImage=new Buffer.from(cover.data,'base64')
