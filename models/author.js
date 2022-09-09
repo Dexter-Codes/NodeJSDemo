ec969f34b5e54bc01b6518c501ab9db57d958db3
Anirban Gupta <anirban.gupta@infor.com>
2022-09-09 22:43:48 +0530

    added edit delete and view for authors

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const Book=require('./book')
 
 const authorSchema = new mongoose.Schema({
         name:
@@ -7,5 +8,24 @@ const authorSchema = new mongoose.Schema({
             required: true
         }
     })
+
+authorSchema.pre('remove', function(next)
+{
+    Book.find({author : this.id}, (err, books)=>
+    {
+        if(err)
+        {
+            next(err)
+        }
+        else if(books.length > 0)
+        {
+            next(new Error('This author still has books added'))
+        }
+        else
+        {
+            next()
+        }
+    })
+})
 
 module.exports=mongoose.model('Author',authorSchema)
