7ed8fc2e76b139c782b6c230b540b12bd694e6c2
Anirban Gupta <anirban.gupta@infor.com>
2022-09-14 01:17:17 +0530

    logging to get error message

--- routes/books.js
+++ routes/books.js
@@ -125,12 +125,12 @@ router.put('/:id',async (req,res)=>
     {
         book =await Book.findById(req.params.id)
         book.title= req.body.title,
-        book.author= req.body.author,
+        book.author= req.body.author.trim(),
         book.publishDate= new Date(req.body.publishDate),
         book.pageCount= req.body.pageCount,
         book.description= req.body.description
 
-        if(req.body.cover!=null && req.body.cover!=='')
+        if(req.body.cover != null && req.body.cover !== '')
         {
             saveCover(book,req.body.cover)
         }
