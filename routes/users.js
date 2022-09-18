5e062808169ccd0b999d20c347b296b2a4a24b9e
Anirban Gupta <anirban.gupta@infor.com>
2022-09-18 22:16:10 +0530

    fix routings

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/', (req,res)=>
     res.render('users/login',{ layout : 'layouts/loginlayout' })
 })
 
-router.post('/', (req,res)=>
+router.get('/go', (req,res)=>
 {
     res.render('/index')
 })
