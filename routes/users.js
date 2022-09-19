7f446a1b55136af4b9c54a758fb505affbe61833
Anirban Gupta <anirban.gupta@infor.com>
2022-09-19 20:05:10 +0530

    add landing page

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,17 @@ const router=express.Router()
 
 router.get('/', (req,res)=>
 {
+    res.render('users/landingpage',{ layout : 'layouts/loginlayout' })
+})
+
+router.get('/login', (req,res)=>
+{
     res.render('users/login',{ layout : 'layouts/loginlayout' })
+})
+
+router.get('/register', (req,res)=>
+{
+    res.render('users/register',{ layout : 'layouts/loginlayout' })
 })
 
 router.get('/go', (req,res)=>
